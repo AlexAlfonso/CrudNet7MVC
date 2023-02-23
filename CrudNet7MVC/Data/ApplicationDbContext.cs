933d557f8bd7e1744935c71586677917abddd3ac
AlexA <AlexA@ARMENTOP>
2023-02-23 04:03:26 +0100

    Agregar archivos de proyecto.

---
+++ CrudNet7MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudNet7MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet7MVC.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+        //Agregar los modelos aquí
+        public DbSet<Contacto> Contactos { get; set; }
+    }
+}
